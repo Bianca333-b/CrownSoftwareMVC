86bfcfa835f7fbcadb6c04be39bc8d5dd6a1e467
Bianca Grobler <groblerbianca97@gmail.com>
2025-09-23 21:33:26 +0200

    updates

--- .Net 8 with MVC/MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ .Net 8 with MVC/MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ced0a6598b17acaf93822fb4c8d3ec04d5fdba96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c506926db2120a83b2485c7e9ad16f0993e4b76d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
