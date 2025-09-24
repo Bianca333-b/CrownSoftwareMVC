a5d6054673707ad82d3186be03135e8fb25756da
Bianca Grobler <groblerbianca97@gmail.com>
2025-09-24 11:34:01 +0200

    Working with database and process images (tut)

--- .Net 8 with MVC/MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ .Net 8 with MVC/MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+015c0fb7e81e527ab19ee2a5c0ac4ffcc6decfd7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f918cfd89d7df7640a71acc9a7c8cfc125e224ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
